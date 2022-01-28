63121801cd92f3cd848aac18e8695f10c75f25d0
reinaldo <reinaldo.coelho@gmail.com>
2022-01-27 22:20:59 -0300

    Adding agregation table and event
    - Adding table to agregate calculated data
    - Add event to process data

--- sample-app/scripts/log_table.sql
+++ sample-app/scripts/log_table.sql
@@ -38,7 +38,17 @@ CREATE TABLE "w3c_log" (
   "user_agent" varchar(255) DEFAULT NULL,
   "user_ipv4" varchar(30) DEFAULT NULL,
   "user_ipv6" varchar(30) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Create agregation table
+  CREATE TABLE w3c_log_agregation (
+      utc_log_date DATETIME NOT NULL,
+      quantity INT NULL,
+      max_time_taken DECIMAL(10,3) NULL, 
+      PRIMARY KEY(utc_log_date)
+  )
+  ENGINE=MyISAM
+  DEFAULT CHARSET=latin1;
 
 
 -- #######################################################################
@@ -51,5 +61,39 @@ ON COMPLETION PRESERVE
 COMMENT 'Clean up Logs at 23:00 daily!'
 DO BEGIN
       DELETE FROM grafana.w3c_log WHERE timestamp < DATE_SUB(NOW(), INTERVAL 7 DAY);
+      DELETE FROM grafana.w3c_log_agregation WHERE utc_log_date < DATE_SUB(NOW(), INTERVAL 7 DAY);
+END;
+-- #######################################################################
+
+
+-- #######################################################################
+-- Create event to clean old data
+-- Usando Event: https://mariadb.com/kb/en/create-event/
+-- #######################################################################
+CREATE OR REPLACE EVENT w3c_data_agregate
+ON SCHEDULE EVERY 1 MINUTE
+ON COMPLETION PRESERVE
+COMMENT 'Agregate w3c APP log data for monitoring!'
+DO BEGIN
+    INSERT INTO w3c_log_agregation(utc_log_date, quantity, max_time_taken)
+    (SELECT
+      FROM_UNIXTIME(UNIX_TIMESTAMP(log_date) DIV 60 * 60),
+      COUNT(*),
+      MAX(time_taken)
+    FROM grafana.w3c_log
+    WHERE
+      log_date BETWEEN
+        (DATE_ADD(NOW(), INTERVAL -5 MINUTE) - INTERVAL EXTRACT(SECOND FROM NOW()) SECOND)
+        AND
+        DATE_ADD((DATE_ADD(NOW(), INTERVAL -1 MINUTE) - INTERVAL EXTRACT(SECOND FROM NOW()) SECOND), INTERVAL 59 SECOND)
+      and Host like 'APP%'
+      and http_status = 200
+      and uri_path NOT LIKE '/signalr%'
+    GROUP BY 1
+    ORDER BY log_date)
+    ON DUPLICATE KEY UPDATE
+      quantity = VALUES(quantity),
+      max_time_taken = VALUES(max_time_taken)
+    ;      
 END;
 -- #######################################################################
